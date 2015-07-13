b5dcaec8aae814daf2d6ec7c09e0d7a986f9f3ff
Decors <decors1215@gmail.com>
2015-07-14 01:22:01 +0900

    first commit

---
+++ Formula/cipherscan.rb
@@ -0,0 +1,16 @@
+class Cipherscan < Formula
+  desc "Retrieve a list of the SSL cipher suites"
+  homepage "https://github.com/jvehent/cipherscan"
+  head "https://github.com/jvehent/cipherscan.git"
+
+  depends_on "bash"
+  depends_on "coreutils"
+
+  def install
+    bin.install "cipherscan", "openssl-darwin64"
+  end
+
+  test do
+    system bin/"cipherscan", "brew.sh"
+  end
+end
---
+++ README.md
@@ -0,0 +1,13 @@
+homebrew-cipherscan
+===============
+
+Homebrew formula for cipherscan.
+
+## Usage
+
+```text
+$ brew tap decors/cipherscan
+$ brew install --HEAD cipherscan
+```
+
+[cipherscan]: https://github.com/jvehent/cipherscan
