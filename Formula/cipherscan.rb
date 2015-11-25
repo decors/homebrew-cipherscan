80a2c20cddee5e8e3aaed479b8af158d25fe69e6
Decors <decors1215@gmail.com>
2015-11-25 17:40:20 +0900

    Update

--- Formula/cipherscan.rb
+++ Formula/cipherscan.rb
@@ -6,6 +6,8 @@ class Cipherscan < Formula
   depends_on "bash"
   depends_on "coreutils"
 
+  patch :DATA
+
   def install
     bin.install "cipherscan", "openssl-darwin64", "analyze.py"
   end
@@ -14,3 +16,18 @@ class Cipherscan < Formula
     system bin/"cipherscan", "brew.sh"
   end
 end
+
+__END__
+diff --git a/cipherscan b/cipherscan
+index 236b34f..2c1fa58 100755
+--- a/cipherscan
++++ b/cipherscan
+@@ -2042,7 +2042,7 @@ fi
+ 
+ if [[ -z $CACERTS ]] && ! [[ -n $CACERTS_ARG_SET ]]; then
+     # find a list of trusted CAs on the local system, or use the provided list
+-    for f in /etc/pki/tls/certs/ca-bundle.crt /etc/ssl/certs/ca-certificates.crt; do
++    for f in /etc/pki/tls/certs/ca-bundle.crt /etc/ssl/certs/ca-certificates.crt /usr/local/etc/openssl/cert.pem; do
+         if [[ -e "$f" ]]; then
+             CACERTS="$f"
+             break
